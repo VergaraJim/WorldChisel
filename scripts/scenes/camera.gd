ce70736d03af5a2de27e9c0d9660a5a96bf1ed50
MSI-VERGARA\Jim Vergara <vergarajim.info@gmail.com>
2026-01-05 16:30:09 +0100

    camera zoom fix

--- scripts/scenes/camera.gd
+++ scripts/scenes/camera.gd
@@ -6,7 +6,7 @@ var camera_start_position
 var dragging = false
 
 var zoom_speed = 0.05
-var min_zoom = 0.1
+var min_zoom = 0.05
 var max_zoom = 2
 
 func _input(event):
@@ -21,10 +21,10 @@ func _input(event):
 				dragging = false
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			if (zoom.x < max_zoom):
-				zoom += Vector2(zoom_speed, zoom_speed)
+				zoom += zoom * zoom_speed
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			if (zoom.x > min_zoom):
-				zoom -= Vector2(zoom_speed, zoom_speed)
+				zoom -= zoom * zoom_speed
 	elif event is InputEventMouseMotion:
 		if dragging:
 			position = camera_start_position + ((mouse_start_pos - event.position) * (1 / zoom.x))
